36c757f92bbbcb62844004362f1a6232c829c4e5
the-anantkumar <f20191046@hyderabad.bits-pilani.ac.in>
2023-06-04 05:22:45 +0530

    commit -  😘🎶😎🤷‍♀️🥵🤠👹😈🙈🐍🫦

--- background.js
+++ background.js
@@ -1,11 +1,18 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "injectCalendarHTML") {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        console.log("TAAABS",tabs);
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          files: ["content.js"],
+
+      chrome.windows.getCurrent({ populate: true }, (window) => {
+        const tabs = window.tabs;
+        console.log(tabs);
+        
+        const need = tabs.filter(tab =>{
+          return tab.active;
         });
+        console.log(need);
+        chrome.scripting.executeScript({
+          target: { tabId: need[0].id },
+          files: ["content.js"],
+        }); // Access the tabs here
       });
     }
   });
